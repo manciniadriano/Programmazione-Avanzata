951b819a14ac3baff6e6152ce2914544e9f4f070
nicolamo <nicolamori0798@gmail.com>
2022-07-16 19:58:30 +0200

    test per vincoli

--- test.js
+++ test.js
@@ -1,5 +1,51 @@
 
 
-var objective = [{ "name": "x1", "start": 3.4, "end": 3.6, "step": 0.1 }, { "name": "x2", "start": 3.4, "end": 3.6, "step": 0.1 }, { "name": "x3", "start": 2.1, "end": 2.3, "step": 0.1 }]
+var objective = [
+    {
+        "name": "c1",
+        "vars": [
+            {
+                "name": "x2",
+                "start": 3.4,
+                "end": 3.5,
+                "step": 0.1
+            }
+        ]
+    },
+    {
+        "name": "c2",
+        "vars": [
+            {
+                "name": "x2",
+                "start": 4.3,
+                "end": 4.4,
+                "step": 0.1
+            }
+        ]
+    }
+]
+
+var array = [];
+objective.map((elem) => {
+    elem.vars.map((item) => {
+        let appoggio = [];
+        let i = Math.round((item.end - item.start) / item.step);
+        for (var n = 0; n <= i; n++) {
+            var object = { name: item.name, value: item.start + item.step * n };
+            appoggio.push(object);
+        }
+        array.push(appoggio);
+    })
+});
+console.log(array);
 
 
+const cartesian = (...f) =>
+    f
+        .map((a) =>
+            a.reduce((a, b) => a.flatMap((d) => b.map((e) => [d, e].flat())))
+        )
+        .flat();
+
+let output = cartesian(array);
+console.log(output);
