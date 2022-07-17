0f2cb4bc62d922bb7820018c14ef564d46266342
msospetti <mattiasospetti@libero.it>
2022-07-17 17:09:29 +0200

    full.sql aggiunti modelli e reviews

--- scripts/full.sql
+++ scripts/full.sql
@@ -23,3 +23,72 @@ CREATE TABLE models(
 INSERT INTO users(email, budget) VALUES
 ('user@user.com', 5),
 ('nicola@nicola.com', 100);
+
+INSERT INTO models(namemodel,objective,subjectto,binaries,options,version,cost,creation_date,valid) VALUES
+('mip_with_binaries',
+'{"direction": 1,"name": "obj","vars": [{"name": "x1","coef": -1},{"name": "x2","coef": -2},{"name": "x3","coef": 0.1},{"name": "x4","coef": 3}]}',
+'[{"name":"c1","vars":[{"name":"x1","coef":1},{"name":"x2","coef":1}],"bnds":{"type":3,"ub":5,"lb":0}},{"name":"c2","vars":[{"name":"x1","coef":2},{"name":"x2","coef":-1}],"bnds":{"type":2,"ub":0,"lb":0}},{"name":"c3","vars":[{"name":"x1","coef":-1},{"name":"x2","coef":3}],"bnds":{"type":2,"ub":0,"lb":0}},{"name":"c4","vars":[{"name":"x3","coef":1},{"name":"x4","coef":1}],"bnds":{"type":2,"ub":0,"lb":0.5}}]',
+'["x3","x4"]',
+'{"mipgap":0,"tmlim":1.7976931348623157e+308,"msglev":1,"presol":true,"cb":{"each":1}}',
+1,
+0.34,
+'7/17/2022',
+true);
+
+INSERT INTO models(namemodel,objective,subjectto,bounds,generals,options,version,cost,creation_date,valid) VALUES
+('mip',
+'{"direction": 2,"name": "obj","vars": [{"name": "x1","coef": 1},{"name": "x2","coef": 2},{"name": "x3","coef": 3},{"name": "x4","coef": 1}]}',
+'[{"name":"c1","vars":[{"name":"x1","coef":-1},{"name":"x2","coef":1},{"name":"x3","coef":1},{"name":"x4","coef":10}],"bnds":{"type":3,"ub":20,"lb":0}},{"name":"c2","vars":[{"name":"x1","coef":1},{"name":"x2","coef":-3},{"name":"x3","coef":1}],"bnds":{"type":3,"ub":30,"lb":0}},{"name":"c3","vars":[{"name":"x2","coef":1},{"name":"x4","coef":-3.5}],"bnds":{"type":5,"ub":0,"lb":0}}]',
+'[{"name": "x1","type": 4,"ub": 40,"lb": 0},{"name": "x4","type": 4,"ub": 3,"lb": 2}]',
+'["x4"]',
+'{"mipgap":0,"tmlim":1.7976931348623157e+308,"msglev":1,"presol":true,"cb":{"each":1}}',
+1,
+0.30,
+'7/17/2022',
+true);
+
+INSERT INTO models(namemodel,objective,subjectto,binaries,options,version,cost,creation_date,valid) VALUES
+('mip_with_binaries',
+'{"direction": 1,"name": "obj","vars": [{"name": "x1","coef": 2},{"name": "x2","coef": 3},{"name": "x3","coef": 4},{"name": "x4","coef": 5}]}',
+'[{"name":"c1","vars":[{"name":"x1","coef":1},{"name":"x2","coef":1}],"bnds":{"type":3,"ub":5,"lb":0}},{"name":"c2","vars":[{"name":"x1","coef":2},{"name":"x2","coef":-1}],"bnds":{"type":2,"ub":0,"lb":0}},{"name":"c3","vars":[{"name":"x1","coef":-1},{"name":"x2","coef":3}],"bnds":{"type":2,"ub":0,"lb":0}},{"name":"c4","vars":[{"name":"x3","coef":1},{"name":"x4","coef":1}],"bnds":{"type":2,"ub":0,"lb":0.5}}]',
+'["x3","x4"]',
+'{"mipgap":0,"tmlim":1.7976931348623157e+308,"msglev":1,"presol":true,"cb":{"each":1}}',
+2,
+0.34,
+'7/17/2022',
+true);
+
+INSERT INTO models(namemodel,objective,subjectto,binaries,options,version,cost,creation_date,valid) VALUES
+('mip_with_binaries',
+'{"direction": 1,"name": "obj","vars": [{"name": "x1","coef": 1},{"name": "x2","coef": 2},{"name": "x3","coef": 3},{"name": "x4","coef": 4}]}',
+'[{"name":"c1","vars":[{"name":"x1","coef":1},{"name":"x2","coef":1}],"bnds":{"type":3,"ub":5,"lb":0}},{"name":"c2","vars":[{"name":"x1","coef":2},{"name":"x2","coef":-1}],"bnds":{"type":2,"ub":0,"lb":0}},{"name":"c3","vars":[{"name":"x1","coef":-1},{"name":"x2","coef":3}],"bnds":{"type":2,"ub":0,"lb":0}},{"name":"c4","vars":[{"name":"x3","coef":1},{"name":"x4","coef":1}],"bnds":{"type":2,"ub":0,"lb":0.5}}]',
+'["x3","x4"]',
+'{"mipgap":0,"tmlim":1.7976931348623157e+308,"msglev":1,"presol":true,"cb":{"each":1}}',
+3,
+0.34,
+'7/17/2022',
+true);
+
+INSERT INTO models(namemodel,objective,subjectto,bounds,generals,options,version,cost,creation_date,valid) VALUES
+('mip',
+'{"direction": 2,"name": "obj","vars": [{"name": "x1","coef": 2},{"name": "x2","coef": 3},{"name": "x3","coef": 4},{"name": "x4","coef": 5}]}',
+'[{"name":"c1","vars":[{"name":"x1","coef":-1},{"name":"x2","coef":1},{"name":"x3","coef":1},{"name":"x4","coef":10}],"bnds":{"type":3,"ub":20,"lb":0}},{"name":"c2","vars":[{"name":"x1","coef":1},{"name":"x2","coef":-3},{"name":"x3","coef":1}],"bnds":{"type":3,"ub":30,"lb":0}},{"name":"c3","vars":[{"name":"x2","coef":1},{"name":"x4","coef":-3.5}],"bnds":{"type":5,"ub":0,"lb":0}}]',
+'[{"name": "x1","type": 4,"ub": 40,"lb": 0},{"name": "x4","type": 4,"ub": 3,"lb": 2}]',
+'["x4"]',
+'{"mipgap":0,"tmlim":1.7976931348623157e+308,"msglev":1,"presol":true,"cb":{"each":1}}',
+2,
+0.30,
+'7/17/2022',
+true);
+
+INSERT INTO models(namemodel,objective,subjectto,bounds,generals,options,version,cost,creation_date,valid) VALUES
+('mip',
+'{"direction": 2,"name": "obj","vars": [{"name": "x1","coef": 3},{"name": "x2","coef": 4},{"name": "x3","coef": 5},{"name": "x4","coef": 6}]}',
+'[{"name":"c1","vars":[{"name":"x1","coef":-1},{"name":"x2","coef":1},{"name":"x3","coef":1},{"name":"x4","coef":10}],"bnds":{"type":3,"ub":20,"lb":0}},{"name":"c2","vars":[{"name":"x1","coef":1},{"name":"x2","coef":-3},{"name":"x3","coef":1}],"bnds":{"type":3,"ub":30,"lb":0}},{"name":"c3","vars":[{"name":"x2","coef":1},{"name":"x4","coef":-3.5}],"bnds":{"type":5,"ub":0,"lb":0}}]',
+'[{"name": "x1","type": 4,"ub": 40,"lb": 0},{"name": "x4","type": 4,"ub": 3,"lb": 2}]',
+'["x4"]',
+'{"mipgap":0,"tmlim":1.7976931348623157e+308,"msglev":1,"presol":true,"cb":{"each":1}}',
+3,
+0.30,
+'7/17/2022',
+true);
