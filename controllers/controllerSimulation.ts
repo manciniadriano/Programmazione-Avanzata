716e51905140cb9a1767bb4b7ee8b21fed0d11f0
nicolamo <nicolamori0798@gmail.com>
2022-07-18 18:43:39 +0200

    Completamento della simulazione

--- controllers/controllerSimulation.ts
+++ controllers/controllerSimulation.ts
@@ -2,40 +2,81 @@ import { getSpecificModel } from "../model/Model";
 import { getBudget } from "../model/User";
 import * as user from "../model/User";
 import * as help from "../middleware/helpFunction/middleModFun";
-import * as helpSim from "./helpSimulation/helpSimulation"
+import * as helpSim from "./helpSimulation/helpSimulation";
 const GLPK = require("glpk.js");
 const glpk = GLPK();
 
 export class SimulationController {
+
+  /**
+   * Funzione per effettuare le simulazioni
+   * @param req request
+   * @param res response
+   */
   public doSimulation = async (req, res) => {
     try {
       let solve: Array<JSON> = [];
       let c: number = 0;
-      if (req.body.objective!== undefined && req.body.subjectTo!== undefined) {
+      //caso in cui vengono specificati sia cambiamenti nella funzione obiettivo che nei vincoli
+      if ( req.body.objective !== undefined && req.body.subjectTo !== undefined) {
         var objectiveVars = combinationFunctionObjective(req.body.objective);
         var subjectToComb = combinationFunctionSubjectTo(req.body.subjectTo);
         var allObject = cartesianComb(objectiveVars, subjectToComb);
         c = 3;
-      } else if (req.body.objective !== undefined) {
-        var objectiveVars = combinationFunctionObjective(req.body.objective);
+      } 
+      //casp in cui viene modificata solamente la funzione obiettivo
+      else if (req.body.objective !== undefined) {
+        var objectiveVars = null;
+        /*controllo per verificare che sia solo un elemento poiché la fuzione cominationFunctionObjective
+        * in tal caso non restituisce un array di array, formato accettato dalla funzione che calcola le soluzioni
+        *
+        */
+        if (req.body.objective.length == 1) {
+          objectiveVars = splitArray(
+            combinationFunctionObjective(req.body.objective)
+          );
+        } else {
+          var objectiveVars = combinationFunctionObjective(req.body.objective);
+        }
         c = 1;
       } else {
+        /**caso analogo per il modifiche solo della obiettivo, in cui se speficato il cambiamento in un unico vincolo
+         * in un'unica variabile non viene creato l'array di array
+         *  
+         * */ 
+        if (req.body.subjectTo.length === 1) {
+          req.body.subjectTo.map((a) => { if (a.vars.length == 1) {
+          subjectToComb = splitArray(combinationFunctionSubjectTo(req.body.subjectTo))
+          } else {subjectToComb = combinationFunctionSubjectTo(req.body.subjectTo)}})
+        } else {
         var subjectToComb = combinationFunctionSubjectTo(req.body.subjectTo);
+        }
         c = 2;
       }
       let model: any = await getSpecificModel(req.body.name, req.body.version);
-      switch(c) {
-        //case 1: helpSim.simulationWithObj(objectiveVars, model, solve); break;
-        //case 2: helpSim.simulationWithSub(subjectToComb, model, solve); break;
-        case 3: helpSim.simulationWithObjSub(allObject, model, solve); break;
+      switch (c) {
+        case 1:
+          helpSim.simulationOnlyObjective(objectiveVars, model, solve);
+          break;
+        case 2:
+          helpSim.simulationOnlySubject(subjectToComb, model, solve);
+          break;
+        case 3:
+          helpSim.simulationWithObjSub(allObject, model, solve);
+          break;
       }
       res.send(solve);
     } catch (e) {
-        res.sendStatus(404);
+      res.sendStatus(404);
     }
   };
 }
 
+/**
+ * Creazione delle combinazioni per le variazioni dei coefficienti della funzione obiettivo
+ * @param objective array col le specifiche di variazione dei coef
+ * @returns combinazioni delle modifiche sui vincoli 
+ */
 const combinationFunctionObjective = (objective: any) => {
   var array = [];
 
@@ -53,6 +94,11 @@ const combinationFunctionObjective = (objective: any) => {
   return output;
 };
 
+/**
+ * Creazione delle combinazioni per le variazioni dei coefficienti dei vincoli
+ * @param objective array col le specifiche di variazione dei coef
+ * @returns combinazioni delle modifiche sui vincoli
+ */
 const combinationFunctionSubjectTo = (objective: any) => {
   var array = [];
   objective.map((elem) => {
@@ -75,5 +121,32 @@ const combinationFunctionSubjectTo = (objective: any) => {
   return output;
 };
 
-const cartesianObjective = (a) => a.reduce((a, b) => a.flatMap(d => b.map((e) => [d, e].flat())));
-const cartesianComb = (array, array2) => array.flatMap((a) => array2.map(b => [a, b].flat()));
+/**
+ * funzione ausiliaria per casi particolari delle combinazioni sia dei vincoli che della funzione obiettivo
+ * @param a array da modificare dei vincoli o della funzione obiettivo
+ * @returns array con giusta formattazione per il calcolo dei modelli
+ */
+const splitArray = (a) => {
+  let array = [];
+  a.map((b) => {
+    array.push([b]);
+  });
+  return array;
+};
+
+/**
+ * prodotto cartesiano per generare le combinazioni nei vincoli e nella funzione obiettivo
+ * @param a array su cui fare le combinazioni
+ * @returns tutte le combinazioni dei coef vincol||funzione obiettivo
+ */
+const cartesianObjective = (a) =>
+  a.reduce((a, b) => a.flatMap((d) => b.map((e) => [d, e].flat())));
+
+/**
+ * Prodotto cartesiano per generare le combinazioni tra i vincoli e la funzione obiettivo
+ * @param array primo array
+ * @param array2 secondo array
+ * @returns combinazioni tra coef dei vincoli e della fun obiettivo
+ */
+const cartesianComb = (array, array2) =>
+  array.flatMap((a) => array2.map((b) => [a, b].flat()));
--- controllers/helpSimulation/helpSimulation.ts
+++ controllers/helpSimulation/helpSimulation.ts
@@ -3,6 +3,157 @@ import { filtraJSON } from "../../middleware/helpFunction/middleModFun";
 const GLPK = require("glpk.js");
 const glpk = GLPK();
 
+
+/**
+ * Funzione per risolvere i modelli dove vengono modificati sia funzione obiettivo che vincoli
+ * @param allObject array di array di oggetti, ogni oggetto contiene una combinazione tra var obiettivo e var vincoli
+ * @param model modello da risolvere
+ * @param solve array dove pushare le soluzioni
+ */
+export const simulationWithObjSub = (allObject, model, solve) => {
+  model = filtraJSON(model);
+  var bestModel: string = null;
+  allObject.map((a) => {
+    model.objective.vars.map((c) => {
+      a.map((b) => {
+        if (b.namesubject === undefined && c.name === b.name) {
+          c.coef = b.value;
+        }
+      });
+    });
+    model.subjectTo.map((c) => {
+      c.vars.map((d) => {
+        a.map((b) => {
+          if (
+            b.namesubject !== undefined &&
+            d.name === b.name &&
+            b.namesubject === c.name
+          ) {
+            d.coef = b.value;
+          }
+        });
+      });
+    });
+    //console.log(JSON.stringify(model));
+    let solution = glpk.solve(model);
+    switch (model.objective.direction) {
+      case 1: {
+        let min = Math.min(...solve.map((item: any) => item.result.z));
+        if (bestModel === null && solve.length === 0) {
+          bestModel = JSON.stringify(model); //assegno con una stringa per non passare il riferimento dell'oggetto
+        } else if (solution.result.z < min) {
+          bestModel = JSON.stringify(model);
+        }
+      }
+      case 2: {
+        let max = Math.max(...solve.map((item: any) => item.result.z));
+        if (bestModel == null && solve.length == 0) {
+          bestModel = JSON.stringify(model);
+        } else if (solution.result.z > max) {
+          bestModel = JSON.stringify(model);
+        }
+      }
+    }
+    solve.push(solution);
+  });
+  bestModel = JSON.parse(bestModel);
+  solve.push(bestModel);
+};
+
+/**
+ * Funzione per risolvere i modelli dove vengono modificati solo i coefficienti della funzione obiettivo
+ * @param objectiveComb combinazioni dei coefficienti dei diversi modelli da calcolare
+ * @param model modello su cui effettuare il calcolo
+ * @param solve array delle soluzioni
+ */
+export const simulationOnlyObjective = (objectiveComb, model, solve) => {
+  model = filtraJSON(model);
+  var bestModel = null;
+  objectiveComb.map((a) => {
+    model.objective.vars.map((c) => {
+      a.map((b) => {
+        if (c.name === b.name) {
+          c.coef = b.value;
+        }
+      });
+    });
+    console.log(JSON.stringify(model));
+    let solution = glpk.solve(model);
+    switch (model.objective.direction) {
+      case 1: {
+        let min = Math.min(...solve.map((item: any) => item.result.z));
+        if (bestModel === null && solve.length === 0) {
+          bestModel = JSON.stringify(model); //assegno con una stringa per non passare il riferimento dell'oggetto
+        } else if (solution.result.z < min) {
+          bestModel = JSON.stringify(model);
+        }
+      }
+      case 2: {
+        let max = Math.max(...solve.map((item: any) => item.result.z));
+        if (bestModel == null && solve.length == 0) {
+          bestModel = JSON.stringify(model);
+        } else if (solution.result.z > max) {
+          bestModel = JSON.stringify(model);
+        }
+      }
+    }
+    solve.push(solution);
+  });
+  bestModel = JSON.parse(bestModel);
+  solve.push(bestModel);
+};
+
+
+/**
+ * Funzione per modificare solamente i coef dei vincoli
+ * @param subjectComb combinazioni dei diversi coefficienti per il modello da calcolare
+ * @param model modello su cui effettuare il calcolo
+ * @param solve array delle soluzioni
+ */
+export const simulationOnlySubject = (subjectComb, model, solve) => {
+  console.log(JSON.stringify(subjectComb))
+  model = filtraJSON(model);
+  var bestModel: string = null;
+  subjectComb.map((a) => {
+    model.subjectTo.map((c) => {
+      c.vars.map((d) => {
+        a.map((b) => {
+          if (
+            b.namesubject !== undefined &&
+            d.name === b.name &&
+            b.namesubject === c.name
+          ) {
+            d.coef = b.value;
+          }
+        });
+      });
+    });
+    //console.log(JSON.stringify(model))
+    let solution = glpk.solve(model);
+    switch (model.objective.direction) {
+      case 1: {
+        let min = Math.min(...solve.map((item: any) => item.result.z));
+        if (bestModel === null && solve.length === 0) {
+          bestModel = JSON.stringify(model); //assegno con una stringa per non passare il riferimento dell'oggetto
+        } else if (solution.result.z < min) {
+          bestModel = JSON.stringify(model);
+        }
+      }
+      case 2: {
+        let max = Math.max(...solve.map((item: any) => item.result.z));
+        if (bestModel == null && solve.length == 0) {
+          bestModel = JSON.stringify(model);
+        } else if (solution.result.z > max) {
+          bestModel = JSON.stringify(model);
+        }
+      }
+    }
+    solve.push(solution);
+  });
+  bestModel = JSON.parse(bestModel);
+  solve.push(bestModel);
+};
+
 /**
  * {
     "name": "lp",
@@ -53,182 +204,3 @@ const glpk = GLPK();
     ]
 }
  */
-export const simulationWithObjSub = (allObject, model, solve) => {
-  model = filtraJSON(model);
-  var bestModel: string = null;
-  allObject.map((a) => {
-    model.objective.vars.map((c) => {
-      a.map((b) => {
-        if (b.namesubject === undefined && c.name === b.name) {
-          c.coef = b.value;
-        }
-      });
-    });
-    model.subjectTo.map((c) => {
-      c.vars.map((d) => {
-        a.map((b) => {
-          if (
-            b.namesubject !== undefined &&
-            d.name === b.name &&
-            b.namesubject === c.name
-          ) {
-            d.coef = b.value;
-          }
-        });
-      });
-    });
-    //console.log(JSON.stringify(model));
-    let solution = glpk.solve(model);
-    console.log(model.objective.direction);
-    switch (model.objective.direction) {
-      case 1: {
-        let min = Math.min(...solve.map((item: any) => item.result.z));
-        if (bestModel === null && solve.length === 0) {
-          bestModel = Object.create(model);
-        } else if (solution.result.z < min) {
-          bestModel = Object.create(model);
-        }
-      }
-      case 2: {
-        let max = Math.max(...solve.map((item: any) => item.result.z));
-        if (bestModel == null && solve.length == 0) {
-          bestModel = JSON.stringify(model);
-        } else if (solution.result.z > max) {
-          bestModel = JSON.stringify(model);
-        }
-      }
-    }
-    solve.push(solution);
-  });
-  solve.push(bestModel);
-};
-/*for (const elem of objectiveVars) {
-    for (const itemelem of elem) {
-      for (const item of model.objective.vars) {
-        if (item.name == itemelem.name) {
-          item["coef"] = itemelem.value;
-        }
-      }
-    }
-    for (const subcomb of subjectToComb) {
-      for (const elem of subcomb) {
-        for (const item of model.subjectTo) {
-          if (item.name == elem.namesubject) {
-            for (const varsub of item.vars) {
-              if (varsub.name == elem.name) {
-                varsub["coef"] = elem.value;
-              }
-            }
-          }
-        }
-      }
-      console.log(
-        "vars model finali obiettivo: " + JSON.stringify(model.objective.vars)
-      );
-      console.log("vars subjectto finali: " + JSON.stringify(model.subjectTo));
-      let solution = glpk.solve(model);
-      
-if (model.objective.direction == 1) {
-      let min = Math.min(...solve.map((item: any) => item.result.z));
-      if (bestModel == null && solve.length == 0) {
-        //stringa per non creare un passaggio del modello per riferimento
-        bestModel = JSON.stringify(model);
-      } else if (solution.result.z < min) {
-        bestModel = JSON.stringify(model);
-      }
-    }
-      if (model.objective.direction == 2) {
-        let max = Math.max(...solve.map((item: any) => item.result.z));
-        if (bestModel == null && solve.length == 0) {
-          bestModel = JSON.stringify(model);
-        } else if (solution.result.z > max) {
-          bestModel = JSON.stringify(model);
-        }
-      }
-      solve.push(solution);
-    }
-  }
-  let object = JSON.parse(bestModel);
-  solve.push(object);
-};*/
-
-/*export const simulationWithObj = (objectiveVars, model, solve) => {
-  var bestModel: string = null;
-  for (const elem of objectiveVars) {
-    for (const item of model.objective.vars) {
-      if (item.name == elem.name) {
-        item["coef"] = elem.value;
-      }
-    }
-    let options = JSON.stringify(model.options);
-    model = filtraJSON(model);
-    console.log(JSON.stringify(model));
-    console.log(options);
-    let solution = glpk.solve(model, options);
-    if (model.objective.direction == 1) {
-      let min = Math.min(...solve.map((item: any) => item.result.z));
-      if (bestModel == null && solve.length == 0) {
-        bestModel = JSON.stringify(model);
-      } else if (solution.result.z < min) {
-        bestModel = JSON.stringify(model);
-      }
-    }
-
-    if (model.objective.direction == 2) {
-      let max = Math.max(...solve.map((item: any) => item.result.z));
-      if (bestModel == null && solve.length == 0) {
-        bestModel = JSON.stringify(model);
-      } else if (solution.result.z > max) {
-        //scelta della stringa poiché con oggetti passaggio per riferimento
-        bestModel = JSON.stringify(model);
-      }
-    }
-    solve.push(solution);
-  }
-  let object = JSON.parse(bestModel);
-
-  solve.push(object);
-};
-
-export const simulationWithSub = (subjectToComb, model, solve) => {
-  var bestModel: string = null;
-  model = filtraJSON(model);
-  for (const subcomb of subjectToComb) {
-    for (const element of subcomb) {
-      for (const item of model.subjectTo) {
-        if (item.name == element.namesubject) {
-          for (const varsub of item.vars) {
-            console.log("model: " + varsub.name + varsub.coef);
-            console.log("vincolo: " + element.name + element.value);
-            if (varsub.name == element.name) {
-              varsub["coef"] = element.value;
-            }
-          }
-        }
-      }
-    }
-    console.log(JSON.stringify(model));
-    let solution = glpk.solve(model);
-    if (model.objective.direction == 1) {
-      let min = Math.min(...solve.map((item: any) => item.result.z));
-      if (bestModel == null && solve.length == 0) {
-        bestModel = JSON.stringify(model);
-      } else if (solution.result.z < min) {
-        bestModel = JSON.stringify(model);
-      }
-    }
-
-    if (model.objective.direction == 2) {
-      let max = Math.max(...solve.map((item: any) => item.result.z));
-      if (bestModel == null && solve.length == 0) {
-        bestModel = JSON.stringify(model);
-      } else if (solution.result.z > max) {
-        //scelta della stringa poiché con oggetti passaggio per riferimento
-        bestModel = JSON.stringify(model);
-      }
-    }
-    solve.push(solution);
-  }
-  let object = JSON.parse(bestModel);
-  solve.push(object);
-};*/
